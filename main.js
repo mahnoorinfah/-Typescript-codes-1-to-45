8baf9910df45a669836a38fa1ae015171d325951
MahnoorShafiq <161619478+mahnoorinfah@users.noreply.github.com>
2024-06-24 04:10:52 -0700

    6 to 10 codes

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
-//Assignment no 1 is node installation which is already installed.
-//Assignment no 2
-//Personal message: store a person's name in a variable , and print a message to
-// that person. Your message should be simple , such as 
-// Hello Eric, would you like to learn some python today?
-var personName = "Eric";
-var msg = " Hello Eric, would you like to learn some python today?";
-console.log(personName, msg);
-//Assignment no 3
-//Name Cases:Store a person's name in a variable ,and then print that person's name
-//in lowercase 'uppercase and titlecase.
-var perName = "MAHANOOR";
-console.log(perName.toUpperCase()); //uppercase
-console.log(perName.toLowerCase()); //lowercase
-//titlecase
-var prName = "i am learning coding";
-var words = prName.split(" ");
-var titleCaseName = "";
-//for loop 1-10
-for (var i = 0; i < words.length; i++) {
-    titleCaseName += words[i].charAt(0).toUpperCase()
-        + words[i].slice(1).toLowerCase() + " ";
-}
-console.log(titleCaseName);
-//Assignment no 4
-//Famous Quote:Find a quote from a famous person you admire.Print the quote and the name of it's author.
-//Your output should look something like the following ,including the quotation marks:
-//Albert Einstein once said,"A person who never made a mistake never tried anything new."
-var famousQuote = "A person who never made a mistake never tried anything new.";
-var author = "Albert Einstein";
-console.log("".concat(author, " once said, \"").concat(famousQuote, "\""));
-//Assignment no 5
-//Famous Quote 2:Repeat exercise 4,but this time store the famous person's name in a variable called
-//famous_person . Then compose your message and store it in a new variable called message. Print your message.
-var famous_person = " Albert Einstein";
-var message = "".concat(famous_person, " once said, \"").concat(famousQuote, "\"");
-console.log(message);
+//Assignment no 6
+//Shipping Names:Store a person's name, and include some whitespace characters at the beginning and end of
+//name.Make sure you use each character combination. "\t" and "\n" , at least once.Print the name once, so
+//the whitespace around the name is displayed then print the name after striping the whitespaces.
+var namewhite_space = "\t \n mahnoor shafiq \t \n";
+console.log("name with white space", namewhite_space);
+var strippedName = namewhite_space.trim();
+console.log("stripped Name:", strippedName);
+//Assignment no 7
+//Number Eight:Write addition,subtraction,multiplication, and division operations that each result in the 8.
+//Be sure to enclose your operations in print statements to se the results.
+var addition = 5 + 3;
+console.log("Addition result:", addition);
+var subtraction = 10 - 2;
+console.log("Subtaction result:", subtraction);
+var multiplication = 4 * 2;
+console.log("Multiply result:", multiplication);
+var division = 80 / 10;
+console.log("Divide result:", division);
+//Assignment no 8
+//You should create  four lines that look like this:
+//console.log(5 + 3)
+//your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(40 / 5);
+//Assignment no 9
+//Favourite Number:Store your favourite no in a variable. Then,using that variable, create a message that 
+//reveals your favourite number. Print that message.
+var favouriteNumber = 6;
+var number_fav = "My favorite number is: ".concat(favouriteNumber);
+console.log(number_fav);
+//Assignment no 10
+//Adding coments:Choose two of the programs you've written ,and add at least one comment to each, if 
+//you don't have anything specific to write because your programs are too simple at this point, just add 
+//your name and the current date at the top of each program file. then write one sentence describing
+//what the program does.
+//program 1
+//firstly declare two variables and assign values 
+var Number1 = 45;
+var Number2 = 5;
+//now divide those  variables value
+var final_result = Number1 / Number2;
+//at last print the result by using console
+console.log(final_result);
+//program 2
+var myHouse = "very beautiful"; //here I declare a variable
+console.log("my house is:", myHouse); //and here I print that
--- main.ts
+++ main.ts
@@ -1,54 +1,72 @@
-//Assignment no 1 is node installation which is already installed.
+//Assignment no 6
+//Shipping Names:Store a person's name, and include some whitespace characters at the beginning and end of
+//name.Make sure you use each character combination. "\t" and "\n" , at least once.Print the name once, so
+//the whitespace around the name is displayed then print the name after striping the whitespaces.
+ let namewhite_space:string = "\t \n mahnoor shafiq \t \n";
+ console.log("name with white space", namewhite_space);     
+ let strippedName:string = namewhite_space.trim();
+ console.log("stripped Name:", strippedName);
 
-//Assignment no 2
-//Personal message: store a person's name in a variable , and print a message to
-// that person. Your message should be simple , such as 
-// Hello Eric, would you like to learn some python today?
+ //Assignment no 7
+ //Number Eight:Write addition,subtraction,multiplication, and division operations that each result in the 8.
+ //Be sure to enclose your operations in print statements to se the results.
 
+ let addition:number = 5+3;
+ console.log("Addition result:", addition);
 
-let personName:string = "Eric";
-let msg = " Hello Eric, would you like to learn some python today?";
+ let subtraction:number = 10-2;
+ console.log("Subtaction result:", subtraction);
 
-console.log(personName, msg);
+ let multiplication:number = 4*2;
+ console.log("Multiply result:", multiplication);
 
-//Assignment no 3
-//Name Cases:Store a person's name in a variable ,and then print that person's name
-//in lowercase 'uppercase and titlecase.
+ let division:number = 80/10;
+ console.log("Divide result:", division);
 
-let perName:string  = "MAHANOOR";
-console.log(perName.toUpperCase()); //uppercase
-console.log(perName.toLowerCase()); //lowercase
-//titlecase
-let prName:string = "i am learning coding";
-let words:string[] = prName.split(" ");
-let titleCaseName: string = "";
+ //Assignment no 8
+ //You should create  four lines that look like this:
+ //console.log(5 + 3)
+ //your output should simply be four lines with the number 8 appearing once on each line.
+ console.log(5 + 3);
+ console.log(10 - 2);
+ console.log(4 * 2);
+ console.log(40 / 5);
 
-//for loop 1-10
-for (let i = 0; i < words.length; i++){
-    titleCaseName += words[i].charAt(0).toUpperCase()
-    + words[i].slice(1).toLowerCase() + " "
-}
-console.log(titleCaseName);
+ //Assignment no 9
+ //Favourite Number:Store your favourite no in a variable. Then,using that variable, create a message that 
+ //reveals your favourite number. Print that message.
 
-//Assignment no 4
-//Famous Quote:Find a quote from a famous person you admire.Print the quote and the name of it's author.
-//Your output should look something like the following ,including the quotation marks:
-//Albert Einstein once said,"A person who never made a mistake never tried anything new."
+ let favouriteNumber:number = 6;
+ let number_fav:string = `My favorite number is: ${favouriteNumber}`;
+ console.log(number_fav);
 
-let famousQuote:string = "A person who never made a mistake never tried anything new.";
-let author:string = "Albert Einstein";
+ //Assignment no 10
+ //Adding coments:Choose two of the programs you've written ,and add at least one comment to each, if 
+ //you don't have anything specific to write because your programs are too simple at this point, just add 
+ //your name and the current date at the top of each program file. then write one sentence describing
+ //what the program does.
 
-console.log(`${author} once said, "${famousQuote}"`);
+ //program 1
+ //firstly declare two variables and assign values 
+ let Number1:number = 45;
+ let Number2:number = 5;
+ //now divide those  variables value
+ let final_result:number = Number1 / Number2;
+ //at last print the result by using console
+ console.log(final_result);
 
-//Assignment no 5
-//Famous Quote 2:Repeat exercise 4,but this time store the famous person's name in a variable called
-//famous_person . Then compose your message and store it in a new variable called message. Print your message.
+ //program 2
 
-let famous_person:string = " Albert Einstein";
-let message: string = `${famous_person} once said, "${famousQuote}"`;
+ let myHouse:string = "very beautiful"; //here I declare a variable
+ 
+ console.log("my house is:", myHouse);//and here I print that
+ 
+ 
 
-console.log(message);
-
+ 
+ 
+ 
+ 
 
 
 
